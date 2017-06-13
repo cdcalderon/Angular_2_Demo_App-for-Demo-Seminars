b7584150a5eb0f4f72273610f63d935fafbb102a
carloscalderon <cdcalderon@gmail.com>
2017-06-13 01:41:44 -0500

    Add ngFor to display multiple Conference Events

--- app/events/event-thumbnail.component.js
+++ app/events/event-thumbnail.component.js
@@ -21,8 +21,8 @@ __decorate([
 EventThumbNailComponent = __decorate([
     core_1.Component({
         selector: 'event-thumbnail',
-        template: "\n        <div class=\"well hoverwell thumbnail\">\n            <h2>{{event.name}}</h2>\n            <div>Date: {{event.time}}</div>\n            <div>Price: ${{event.price}}</div>\n            <div>\n                <span>Location {{event.location.address}}</span>\n                <span class=\"pad-left\">{{event.location.city}}, {{event.location.country}}</span>\n            </div>\n        </div>\n    ",
-        styles: ["\n        .pad-left {margin-left: 10px}\n        .well div { color: #bbb;}\n    "]
+        template: "\n        <div class=\"well hoverwell thumbnail\">\n            <h2>{{event?.name}}</h2>\n            <div>Date: {{event?.time}}</div>\n            <div>Price: ${{event?.price}}</div>\n            <div>\n                <span>Location {{event?.location?.address}}</span>\n                <span class=\"pad-left\">{{event?.location?.city}}, {{event?.location?.country}}</span>\n            </div>\n            <div>\n                Online URL: {{event?.onlineUrl}}\n            </div>\n        </div>\n    ",
+        styles: ["\n        .pad-left {margin-left: 10px}\n        .well div { color: #bbb;}\n        .thumbnail {min-height: 210px}\n    "]
     }),
     __metadata("design:paramtypes", [])
 ], EventThumbNailComponent);
